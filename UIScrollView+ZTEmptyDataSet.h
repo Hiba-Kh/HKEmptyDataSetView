f864d4739c69fe2c048454c226cf120b5bc5699c
HibaAmeen <khalifahhiba@gmail.com>
2019-04-15 20:00:27 +0300

    Adding empty data set view with swizzling and clickable buttton

---
+++ UIScrollView+ZTEmptyDataSet.h
@@ -0,0 +1,44 @@
+//
+//  UIScrollView+HibaEmptyDataSet.h
+//  MedSolace
+//
+//  Created by Hiba Khalifah on 3/11/19.
+//  Copyright © 2019 MedSolace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol ZTEmptyDataSetSource <NSObject>
+
+@optional
+
+- (UIImage *)imageForEmptyDataSet:(UIScrollView *)scrollView;
+- (NSAttributedString *)titleForEmptyDataSet:(UIScrollView *)scrollView;
+- (NSAttributedString *)descriptionForEmptyDataSet:(UIScrollView *)scrollView;
+- (UIColor *)backgroundColorForEmptyDataSet:(UIScrollView *)scrollView;
+- (CGFloat)verticalOffsetForEmptyDataSet:(UIScrollView *)scrollView;
+- (BOOL)emptyDataSet:(UIScrollView *)scrollView enableEmptyDataSetButton:(UIButton *)button;
+
+@end
+
+@protocol ZTEmptyDataSetDelegate <NSObject>
+
+@optional
+
+- (void)emptyDataSet:(UIScrollView *)scrollView didTapButton:(UIButton *)button;
+- (UIImage *)buttonImageForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state;
+
+@end
+
+@interface UIScrollView (ZTEmptyDataSet)
+
+@property (nonatomic, nullable, weak) IBOutlet id <ZTEmptyDataSetSource> emptyDataSetSource;
+@property (nonatomic, nullable, weak) IBOutlet id <ZTEmptyDataSetDelegate> emptyDataSetDelegate;
+
+- (void)reloadEmptyDataSet;
+
+@end
+NS_ASSUME_NONNULL_END
